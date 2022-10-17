e0c4e5cafa3d72ea20a03003f4bd5f18497e9342
Romain857 <romainmart.85@gmail.com>
2022-10-17 17:25:06 +0200

    maj main sur main

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
+from main import MachineACafe
+
+
 class TestMachineCafe():
     def argentRendu():
         cafe = 0
         argent = 40
+        machineACafe = MachineACafe()
         
+        if cafe == 0:
+            machineACafe
         
