fc232b332bdb40cd4596e7b04c49dfb2e889ce15
gabriel <gabriel.filiot@epsi.fr>
2022-10-17 17:52:16 +0200

    test2

---
+++ test2.py
@@ -0,0 +1,18 @@
+import unittest
+from fonction import MachineACafe
+
+class TestPython(unittest.TestCase):
+    global argent 
+    argent = 40
+
+    def test_coffee_coule(self):
+        self.assertEqual(40, argent)
+        self.assertTrue(MachineACafe.servirCafe())
+
+    def test_coffee_tasse(self):
+
+        self.assertEqual(40, argent)
+        self.assertFalse(MachineACafe.donnerGobelet())
+
+if __name__ == '__main__':
+    unittest.main()
